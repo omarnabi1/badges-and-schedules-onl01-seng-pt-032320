bf9eec5e3cd53d382694ea186a8a43652a8bc283
Omar Nabi <onabi20@gmail.com>
2020-03-31 03:06:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(attendees)
     badge_maker(name)
   end
 end
+
+def assign_rooms(attendees)
+  room=0 
+  attendees.collect do |name|
+    room+1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
